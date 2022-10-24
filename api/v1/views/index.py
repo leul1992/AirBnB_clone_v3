d713267e52000ac30e8e052deaf206bd73e56898
Leulseged <leulbekele191@gmail.com>
2022-10-25 01:10:19 +0300

    fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,13 +11,13 @@ from models.user import User
 from models import storage
 
 
-@app_views.route('/status')
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """returns status of API"""
     return jsonify({'status': 'OK'})
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
     "retrieves the number of each objects by type"
     return jsonify({
