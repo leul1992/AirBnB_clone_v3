04f43d726e8af3b2bf7087c7791a6bb10447f09f
Leulseged <leulbekele191@gmail.com>
2022-10-25 13:31:09 +0300

    fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,6 +10,8 @@ from api.v1.views import app_views
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
+
+
 @app.teardown_appcontext
 def teardown(Exception):
     """closes the storage on teardown"""
