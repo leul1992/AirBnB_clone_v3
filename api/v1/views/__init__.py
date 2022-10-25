9e16d9bd7ca2ea396290de52090d41a5263e9cf9
Leulseged <leulbekele191@gmail.com>
2022-10-25 11:36:28 +0300

    fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """contains app_views blueprint"""
 from flask import Blueprint
+
+app_views = Blueprint('api', __name__, url_prefix='/api/v1')
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.users import *
-
-
-app_views = Blueprint('api', __name__, url_prefix='/api/v1')
