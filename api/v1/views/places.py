8ccb525b2ebd8035db32fc0860f90449b14bcef0
Leulseged <leulbekele191@gmail.com>
2022-10-30 17:28:26 +0300

    fix

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,124 +1,80 @@
 #!/usr/bin/python3
-"""Places view"""
-
-from api.v1.views import app_views
+"""Contains all REST actions for place Objects"""
 from flask import abort, jsonify, make_response, request
-from models import storage
-from models.city import City
+from api.v1.views import app_views
 from models.place import Place
 from models.user import User
+from models import storage
+from models.city import City
 
 
-@app_views.route('/cities/<string:city_id>/places', methods=['GET'],
+@app_views.route('/cities/<city_id>/places', methods=['GET'],
                  strict_slashes=False)
-def get_places(city_id):
-    """Fetch place information"""
+def places(city_id):
+    """retrieves a list of all place objects of city"""
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
-    places = []
-    for place in city.places:
-        places.append(place.to_dict())
-    return jsonify(places)
+    places = city.places
+    return jsonify([val.to_dict() for val in places])
 
 
-@app_views.route('/places/<string:place_id>', methods=['GET'],
-                 strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
 def get_place(place_id):
-    """Fetch place information for specified place"""
+    """retrieves a place objects"""
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
     return jsonify(place.to_dict())
 
 
-@app_views.route('/places/<string:place_id>', methods=['DELETE'],
+@app_views.route('/places/<place_id>', methods=['DELETE'],
                  strict_slashes=False)
-def delete_place(place_id):
-    """Delete a place based on its place_id"""
+def del_place(place_id):
+    """deletes a place objects"""
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
-    place.delete()
+    storage.delete(place)
     storage.save()
-    return (jsonify({}))
+    return jsonify({})
 
 
-@app_views.route('/cities/<string:city_id>/places', methods=['POST'],
+@app_views.route('/cities/<city_id>/places', methods=['POST'],
                  strict_slashes=False)
-def post_place(city_id):
-    """Create a new place"""
+def new_place(city_id):
+    """creates a place objects"""
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
-    if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    kwargs = request.get_json()
-    if 'user_id' not in kwargs:
-        return make_response(jsonify({'error': 'Missing user_id'}), 400)
-    user = storage.get(User, kwargs['user_id'])
+    if not request.json:
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+    if 'user_id' not in request.json:
+        return make_response(jsonify({"error": "Missing user_id"}), 400)
+    user = storage.get(User, request.json['user_id'])
     if user is None:
         abort(404)
-    if 'name' not in kwargs:
-        return make_response(jsonify({'error': 'Missing name'}), 400)
-    kwargs['city_id'] = city_id
-    place = Place(**kwargs)
+    if 'name' not in request.json:
+        return make_response(jsonify({"error": "Missing name"}), 400)
+    data = request.get_json()
+    data['city_id'] = city_id
+    place = Place(**data)
     place.save()
     return make_response(jsonify(place.to_dict()), 201)
 
 
-@app_views.route('/places/<string:place_id>', methods=['PUT'],
-                 strict_slashes=False)
-def put_place(place_id):
-    """Update a place"""
+@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
+def update_place(place_id):
+    """updates a place objects"""
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
-    if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    for attr, val in request.get_json().items():
-        if attr not in ['id', 'user_id', 'city_id', 'created_at',
-                        'updated_at']:
-            setattr(place, attr, val)
+    if not request.json:
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+    data = {k: v for k, v in request.get_json().items()
+            if k not in ['id', 'user_id', 'city_id', 'created_at',
+            'updated_at']}
+    for k, v in data.items():
+        setattr(place, k, v)
     place.save()
     return jsonify(place.to_dict())
-
-
-@app_views.route('/places_search', methods=['POST'], strict_slashes=False)
-def post_places_search():
-    """Searche for place"""
-    if request.get_json() is not None:
-        params = request.get_json()
-        states = params.get('states', [])
-        cities = params.get('cities', [])
-        amenities = params.get('amenities', [])
-        amenity_objects = []
-        for amenity_id in amenities:
-            amenity = storage.get(Amenity, amenity_id)
-            if amenity:
-                amenity_objects.append(amenity)
-        if states == cities == []:
-            places = storage.all(Place).values()
-        else:
-            places = []
-            for state_id in states:
-                state = storage.get(State, state_id)
-                state_cities = state.cities
-                for city in state_cities:
-                    if city.id not in cities:
-                        cities.append(city.id)
-            for city_id in cities:
-                city = storage.get('City', city_id)
-                for place in city.places:
-                    places.append(place)
-        confirmed_places = []
-        for place in places:
-            place_amenities = place.amenities
-            confirmed_places.append(place.to_dict())
-            for amenity in amenity_objects:
-                if amenity not in place_amenities:
-                    confirmed_places.pop()
-                    break
-        return jsonify(confirmed_places)
-    else:
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
